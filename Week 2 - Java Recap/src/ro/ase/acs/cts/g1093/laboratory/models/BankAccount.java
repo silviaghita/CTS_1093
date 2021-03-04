0e0ac9423e35ee3b56456435391ccee9a0e7e988
silvi <silviaghita99@gmail.com>
2021-03-04 15:28:38 +0200

    Lab 2 commit

---
+++ Week 2 - Java Recap/src/ro/ase/acs/cts/g1093/laboratory/models/BankAccount.java
@@ -0,0 +1,16 @@
+package ro.ase.acs.cts.g1093.laboratory.models;
+
+public abstract class BankAccount extends Account {
+	protected String iban;
+	protected double balance;
+	
+	public BankAccount(String iban, double balance) {
+		this.balance = balance;
+		this.iban = iban;
+	}
+	
+	@Override
+	public double getBalance() {
+		return this.balance;
+	}
+}
