9f98dd2d9ef479036e89b11ab5dcdc0688e97f3f
silvi <silviaghita99@gmail.com>
2021-03-25 14:32:29 +0200

    Singleton - Standard implementation

---
+++ Week5- Singleton/src/ro/ase/csie/cts/g1093/dp/singleton/UIModule.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1093.dp.singleton;
+
+public class UIModule {
+	
+	public UIModule() {
+		
+		//RESTBackend backend = new RESTBackend("acs.ase.ro/cts", "get some data");
+		RESTBackend backend = RESTBackend.getRESTBackend();
+	}
+}
