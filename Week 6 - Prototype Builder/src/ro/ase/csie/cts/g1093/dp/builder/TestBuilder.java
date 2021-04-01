3beea558f7e117a52a34556fdffab312f2527419
silvi <silviaghita99@gmail.com>
2021-04-01 14:33:02 +0300

    Prototype implementation. Starting builder

---
+++ Week 6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1093.dp.builder;
+
+public class TestBuilder {
+
+	public static void main(String[] args) {
+		
+		//1. create the object
+		SuperHero superHero = new SuperHero();
+		//2. initialize it
+		//TO DO: don't forget
+		
+		SuperHero superHero2 = new SuperHero("Spiderman", 100, false, false, new Weapon(), null, new Flying(), null);
+
+	}
+
+}
---
+++ Week 6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/Weapon.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1093.dp.builder;
+
+public class Weapon implements WeaponInterface {
+
+	@Override
+	public void pewPew() {
+		System.out.println("-> -> -> -> -> ->");
+		
+	}
+	
+	
+
+}
