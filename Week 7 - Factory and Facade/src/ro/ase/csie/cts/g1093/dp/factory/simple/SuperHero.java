6b470834a4bddff2f072191074ddf06de7bf4bd6
silvi <silviaghita99@gmail.com>
2021-04-08 14:32:30 +0300

    Implementing Simple Factory

---
+++ Week 7 - Factory and Facade/src/ro/ase/csie/cts/g1093/dp/factory/simple/SuperHero.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1093.dp.factory.simple;
+
+public class SuperHero {
+	String name;
+	
+
+}
---
+++ Week 7 - Factory and Facade/src/ro/ase/csie/cts/g1093/dp/factory/simple/TestFactory.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.g1093.dp.factory.simple;
+
+public class TestFactory {
+
+	public static void main(String[] args) {
+		
+//		AbstractWeapon pistol = new Pistol("black", 100);
+//		AbstractWeapon machineGun = new MachineGun("red");
+//		AbstractWeapon bazooka = new Bazooka("green", 1000, 500);
+		
+		AbstractWeapon weapon = WeaponFactory.getWeapon(WeaponType.PISTOL, "black");
+		weapon = WeaponFactory.getWeapon(WeaponType.MACHINE_GUN, "red");
+		weapon = WeaponFactory.getWeapon(WeaponType.BAZOOKA, "green");
+
+	}
+
+}
